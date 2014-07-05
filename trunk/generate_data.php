517677eab14622b2234dbaf257f92bfb46533e22
tomholland <tomholland@d28b2937-92ed-460d-83c9-1728ac47e403>
2014-07-05 15:37:24 +0000

    Added data from spreadsheet

--- trunk/generate_data.php
+++ trunk/generate_data.php
@@ -7,48 +7,1129 @@ $data = array(
 			'image' => 'silvermoon.jpg',
 			'characters' => array(
 				array(
-					'name' => 'Dave',
-					'rice' => 'Su',
+					'name' => 'Wakasu Akuma',
+					'rice' => 1,
 					'cards' => array(
-						'1.jpg',
-						'2.jpg'
+						'130-1.jpg',
+						'130-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Okkete San',
+					'rice' => 2,
+					'cards' => array(
+						'131-1.jpg',
+						'131-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kyoaku-Han',
+					'rice' => 4,
+					'cards' => array(
+						'126-1.jpg',
+						'126-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Oda',
+					'rice' => 5,
+					'cards' => array(
+						'122-1.jpg',
+						'122-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kyoaku-Han Crossbowman',
+					'rice' => 5,
+					'cards' => array(
+						'125-1.jpg',
+						'125-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Sukuratchi',
+					'rice' => 5,
+					'cards' => array(
+						'129-1.jpg',
+						'129-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Old Zo',
+					'rice' => 5,
+					'cards' => array(
+						'127-1.jpg',
+						'127-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Senpu',
+					'rice' => 6,
+					'cards' => array(
+						'134-1.jpg',
+						'134-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Wasupu',
+					'rice' => 6,
+					'cards' => array(
+						'135-1.jpg',
+						'135-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Yukio Koshimori',
+					'rice' => 7,
+					'cards' => array(
+						'9-1.jpg',
+						'9-2.jpg',
+						'10-1.jpg',
+						'10-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Saki, Rose of Jima',
+					'rice' => 7,
+					'cards' => array(
+						'124-1.jpg',
+						'124-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Tsubaki',
+					'rice' => 7,
+					'cards' => array(
+						'136-1.jpg',
+						'136-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Golden Sentinel',
+					'rice' => 8,
+					'cards' => array(
+						'3-1.jpg',
+						'3-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Harukichi',
+					'rice' => 8,
+					'cards' => array(
+						'132-1.jpg',
+						'132-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Fitiaumua',
+					'rice' => 9,
+					'cards' => array(
+						'128-1.jpg',
+						'128-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Manu',
+					'rice' => 9,
+					'cards' => array(
+						'133-1.jpg',
+						'133-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Tautolu',
+					'rice' => 10,
+					'cards' => array(
+						'123-1.jpg',
+						'123-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Oda & Tautolu',
+					'rice' => 12,
+					'cards' => array(
+						'121-1.jpg',
+						'121-2.jpg'
 					)
 				)
 			)
 		),
 		array(
-			'name' => 'Ito Clan',
+			'name' => 'The Ito Clan',
 			'image' => 'Itosymbol.jpg',
 			'characters' => array(
-				
+				array(
+					'name' => 'Kazuhiko',
+					'rice' => 4,
+					'cards' => array(
+						'112-1.jpg',
+						'112-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Hitoshi',
+					'rice' => 5,
+					'cards' => array(
+						'107-1.jpg',
+						'107-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Temple Bushi',
+					'rice' => 5,
+					'cards' => array(
+						'117-1.jpg',
+						'117-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Akimoto',
+					'rice' => 5,
+					'cards' => array(
+						'119-1.jpg',
+						'119-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Jade Mamba Guard',
+					'rice' => 6,
+					'cards' => array(
+						'108-1.jpg',
+						'108-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Chiyo, Temple Bushi',
+					'rice' => 6,
+					'cards' => array(
+						'118-1.jpg',
+						'118-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Yukio Koshimori',
+					'rice' => 7,
+					'cards' => array(
+						'8-1.jpg',
+						'8-2.jpg',
+						'10-1.jpg',
+						'10-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Takeji',
+					'rice' => 7,
+					'cards' => array(
+						'111-1.jpg',
+						'111-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Sakura',
+					'rice' => 7,
+					'cards' => array(
+						'120-1.jpg',
+						'120-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Golden Sentinel',
+					'rice' => 8,
+					'cards' => array(
+						'2-1.jpg',
+						'2-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Naoko',
+					'rice' => 8,
+					'cards' => array(
+						'113-1.jpg',
+						'113-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Masunagi Ito',
+					'rice' => 9,
+					'cards' => array(
+						'109-1.jpg',
+						'109-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kaihime Ito',
+					'rice' => 9,
+					'cards' => array(
+						'110-1.jpg',
+						'110-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Satoshi',
+					'rice' => 9,
+					'cards' => array(
+						'115-1.jpg',
+						'115-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kenzo Ito',
+					'rice' => 10,
+					'cards' => array(
+						'114-1.jpg',
+						'114-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Ayako Ito',
+					'rice' => 10,
+					'cards' => array(
+						'116-1.jpg',
+						'116-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Yatsumata',
+					'rice' => 11,
+					'cards' => array(
+						'106-1.jpg',
+						'106-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Saburo',
+					'rice' => 14,
+					'cards' => array(
+						'105-1.jpg',
+						'105-2.jpg'
+					)
+				)
 			)
 		),
 		array(
 			'name' => 'Temple of Ro-Kan',
 			'image' => 'symbolmonk.jpg',
 			'characters' => array(
-				
+				array(
+					'name' => 'Koji\'s Pack',
+					'rice' => 1,
+					'cards' => array(
+						'46-1.jpg',
+						'46-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Rice Farmers of the Rokan',
+					'rice' => 3,
+					'cards' => array(
+						'53-1.jpg',
+						'53-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Fishermen of the Rokan',
+					'rice' => 4,
+					'cards' => array(
+						'36-1.jpg',
+						'36-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kami of the Strong West Wind',
+					'rice' => 4,
+					'cards' => array(
+						'37-1.jpg',
+						'37-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kami of the Eternal Earth',
+					'rice' => 4,
+					'cards' => array(
+						'38-1.jpg',
+						'38-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kami of the Evening Flame',
+					'rice' => 4,
+					'cards' => array(
+						'41-1.jpg',
+						'41-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kami of the Morning Dew',
+					'rice' => 4,
+					'cards' => array(
+						'42-1.jpg',
+						'42-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Aiko',
+					'rice' => 4,
+					'cards' => array(
+						'50-1.jpg',
+						'50-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Atsuko',
+					'rice' => 4,
+					'cards' => array(
+						'54-1.jpg',
+						'54-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kintaru',
+					'rice' => 5,
+					'cards' => array(
+						'40-1.jpg',
+						'40-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Koji',
+					'rice' => 5,
+					'cards' => array(
+						'45-1.jpg',
+						'45-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Hisao',
+					'rice' => 6,
+					'cards' => array(
+						'32-1.jpg',
+						'32-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Hotaru',
+					'rice' => 6,
+					'cards' => array(
+						'44-1.jpg',
+						'44-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Seiji',
+					'rice' => 7,
+					'cards' => array(
+						'31-1.jpg',
+						'31-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Tsutsumi',
+					'rice' => 7,
+					'cards' => array(
+						'33-1.jpg',
+						'33-2.jpg'
+					)
+				),
+				array(
+					'name' => 'The Guardian',
+					'rice' => 7,
+					'cards' => array(
+						'49-1.jpg',
+						'49-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kawa no Rojin',
+					'rice' => 8,
+					'cards' => array(
+						'39-1.jpg',
+						'39-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Riku',
+					'rice' => 8,
+					'cards' => array(
+						'47-1.jpg',
+						'47-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kitsune',
+					'rice' => 9,
+					'cards' => array(
+						'51a-1.jpg',
+						'51a-2.jpg',
+						'51b-1.jpg',
+						'51b-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Yumi',
+					'rice' => 9,
+					'cards' => array(
+						'52-1.jpg',
+						'52-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kenko',
+					'rice' => 9,
+					'cards' => array(
+						'56-1.jpg',
+						'56-2.jpg'
+					)
+				),
+				array(
+					'name' => 'The Grey Pilgrim',
+					'rice' => 10,
+					'cards' => array(
+						'5-1.jpg',
+						'5-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Suchiro',
+					'rice' => 10,
+					'cards' => array(
+						'35-1.jpg',
+						'35-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Shisa',
+					'rice' => 10,
+					'cards' => array(
+						'48-1.jpg',
+						'48-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Master Ekusa',
+					'rice' => 11,
+					'cards' => array(
+						'55-1.jpg',
+						'55-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Master Po',
+					'rice' => 12,
+					'cards' => array(
+						'43-1.jpg',
+						'43-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Sojobo',
+					'rice' => 13,
+					'cards' => array(
+						'34-1.jpg',
+						'34-2.jpg'
+					)
+				)
 			)
 		),
 		array(
 			'name' => 'Prefecture of Ryu',
 			'image' => 'symbolsamurai.jpg',
 			'characters' => array(
-				
+				array(
+					'name' => 'Guardsman of Ryu',
+					'rice' => 4,
+					'cards' => array(
+						'11-1.jpg',
+						'11-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Madoka',
+					'rice' => 4,
+					'cards' => array(
+						'13-1.jpg',
+						'13-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Daisuke',
+					'rice' => 4,
+					'cards' => array(
+						'22-1.jpg',
+						'22-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Junichi',
+					'rice' => 5,
+					'cards' => array(
+						'19-1.jpg',
+						'19-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Minuro',
+					'rice' => 5,
+					'cards' => array(
+						'27-1.jpg',
+						'27-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Ryu Yarimen',
+					'rice' => 5,
+					'cards' => array(
+						'28-1.jpg',
+						'28-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Fujitaka',
+					'rice' => 6,
+					'cards' => array(
+						'17-1.jpg',
+						'17-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Matsu Takashi',
+					'rice' => 6,
+					'cards' => array(
+						'23-1.jpg',
+						'23-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Jin',
+					'rice' => 6,
+					'cards' => array(
+						'26-1.jpg',
+						'26-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Isamu',
+					'rice' => 7,
+					'cards' => array(
+						'6-1.jpg',
+						'6-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Yukio Koshimori',
+					'rice' => 7,
+					'cards' => array(
+						'7-1.jpg',
+						'7-2.jpg',
+						'10-1.jpg',
+						'10-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Mikio',
+					'rice' => 7,
+					'cards' => array(
+						'24-1.jpg',
+						'24-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Golden Sentinel',
+					'rice' => 8,
+					'cards' => array(
+						'1-1.jpg',
+						'1-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Tenchi',
+					'rice' => 8,
+					'cards' => array(
+						'20-1.jpg',
+						'20-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Genji Takashi',
+					'rice' => 9,
+					'cards' => array(
+						'14-1.jpg',
+						'14-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Eiji Takashi',
+					'rice' => 9,
+					'cards' => array(
+						'15-1.jpg',
+						'15-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Bikou',
+					'rice' => 9,
+					'cards' => array(
+						'25-1.jpg',
+						'25-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Hanso',
+					'rice' => 9,
+					'cards' => array(
+						'30-1.jpg',
+						'30-2.jpg'
+					)
+				),
+				array(
+					'name' => 'The Grey Pilgrim',
+					'rice' => 10,
+					'cards' => array(
+						'4-1.jpg',
+						'4-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Ryoko-sha',
+					'rice' => 10,
+					'cards' => array(
+						'16-1.jpg',
+						'16-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Hagane Takashi',
+					'rice' => 10,
+					'cards' => array(
+						'18-1.jpg',
+						'18-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Airo Takashi',
+					'rice' => 11,
+					'cards' => array(
+						'29-1.jpg',
+						'29-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Karsumoto Takashi',
+					'rice' => 12,
+					'cards' => array(
+						'12-1.jpg',
+						'12-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Mizuchi',
+					'rice' => 14,
+					'cards' => array(
+						'21-1.jpg',
+						'21-2.jpg'
+					)
+				),
 			)
 		),
 		array(
-			'name' => 'Cult of Yurei',
+			'name' => 'The Cult of Yurei',
 			'image' => 'symbolyoma.jpg',
 			'characters' => array(
-				
+				array(
+					'name' => 'Rat Swarm',
+					'rice' => 1,
+					'cards' => array(
+						'75-1.jpg',
+						'75-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kusatta Kairai',
+					'rice' => 4,
+					'cards' => array(
+						'59-1.jpg',
+						'59-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kairai Farmer',
+					'rice' => 4,
+					'cards' => array(
+						'80-1.jpg',
+						'80-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kairai Militia',
+					'rice' => 4,
+					'cards' => array(
+						'81-1.jpg',
+						'81-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kairai Villager',
+					'rice' => 5,
+					'cards' => array(
+						'66-1.jpg',
+						'66-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Shichiro',
+					'rice' => 5,
+					'cards' => array(
+						'67-1.jpg',
+						'67-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Gengo',
+					'rice' => 5,
+					'cards' => array(
+						'70-1.jpg',
+						'70-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Nezumi Kun',
+					'rice' => 6,
+					'cards' => array(
+						'60-1.jpg',
+						'60-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Gaki',
+					'rice' => 6,
+					'cards' => array(
+						'62-1.jpg',
+						'62-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Large Swarm of Rats',
+					'rice' => 6,
+					'cards' => array(
+						'68-1.jpg',
+						'68-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Armoured Kairai',
+					'rice' => 6,
+					'cards' => array(
+						'79-1.jpg',
+						'79-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Nezumi',
+					'rice' => 7,
+					'cards' => array(
+						'74a-1.jpg',
+						'74a-2.jpg',
+						'74b-1.jpg',
+						'74b-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Tadao',
+					'rice' => 8,
+					'cards' => array(
+						'57-1.jpg',
+						'57-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Mo Ises',
+					'rice' => 8,
+					'cards' => array(
+						'64-1.jpg',
+						'64-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Taka',
+					'rice' => 8,
+					'cards' => array(
+						'71-1.jpg',
+						'71-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Taka: Wrath',
+					'rice' => 'Su',
+					'cards' => array(
+						'72-1.jpg',
+						'72-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Wanyudo',
+					'rice' => 9,
+					'cards' => array(
+						'61-1.jpg',
+						'61-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Harionago',
+					'rice' => 9,
+					'cards' => array(
+						'63-1.jpg',
+						'63-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Araka',
+					'rice' => 9,
+					'cards' => array(
+						'73-1.jpg',
+						'73-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Ikiryo',
+					'rice' => 10,
+					'cards' => array(
+						'78-1.jpg',
+						'78-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Waku, Soul Collector',
+					'rice' => 11,
+					'cards' => array(
+						'58a-1.jpg',
+						'58a-2.jpg',
+						'58b-1.jpg',
+						'58b-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Gok, the Corpse Collector',
+					'rice' => 11,
+					'cards' => array(
+						'69-1.jpg',
+						'69-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kato, the Puppet Master',
+					'rice' => 11,
+					'cards' => array(
+						'76a-1.jpg',
+						'76a-2.jpg',
+						'76b-1.jpg',
+						'76b-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kato: Marionette',
+					'rice' => 'Su',
+					'cards' => array(
+						'77-1.jpg',
+						'77-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Wraith',
+					'rice' => 12,
+					'cards' => array(
+						'65-1.jpg',
+						'65-2.jpg'
+					)
+				)
 			)
 		),
 		array(
 			'name' => 'Savage Wave',
 			'image' => 'symboloni.jpg',
 			'characters' => array(
-				
+				array(
+					'name' => 'Bakemono Horde',
+					'rice' => '-',
+					'cards' => array(
+						'100-1.jpg',
+						'100-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Oni Slave',
+					'rice' => 3,
+					'cards' => array(
+						'91-1.jpg',
+						'91-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Jun, Oni Slave',
+					'rice' => 4,
+					'cards' => array(
+						'87-1.jpg',
+						'87-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Bakemono Archers',
+					'rice' => 4,
+					'cards' => array(
+						'101-1.jpg',
+						'101-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Bakemono Beaters',
+					'rice' => 4,
+					'cards' => array(
+						'102-1.jpg',
+						'102-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Bakemono Spearmen',
+					'rice' => 4,
+					'cards' => array(
+						'103-1.jpg',
+						'103-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Tribal Brute',
+					'rice' => 5,
+					'cards' => array(
+						'84-1.jpg',
+						'84-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Tra Peng',
+					'rice' => 5,
+					'cards' => array(
+						'93-1.jpg',
+						'93-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Okina & Oto',
+					'rice' => 5,
+					'cards' => array(
+						'96-1.jpg',
+						'96-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Zung Fu',
+					'rice' => 6,
+					'cards' => array(
+						'85-1.jpg',
+						'85-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Uk-Kang',
+					'rice' => 6,
+					'cards' => array(
+						'89-1.jpg',
+						'89-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Giant Cave Bat',
+					'rice' => 7,
+					'cards' => array(
+						'86-1.jpg',
+						'86-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Wu-Zang',
+					'rice' => 7,
+					'cards' => array(
+						'98-1.jpg',
+						'98-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Boba',
+					'rice' => 8,
+					'cards' => array(
+						'88-1.jpg',
+						'88-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kano',
+					'rice' => 8,
+					'cards' => array(
+						'90-1.jpg',
+						'90-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kaihei Alpha',
+					'rice' => 9,
+					'cards' => array(
+						'82-1.jpg',
+						'82-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Waka',
+					'rice' => 10,
+					'cards' => array(
+						'97-1.jpg',
+						'97-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Nian',
+					'rice' => 10,
+					'cards' => array(
+						'92-1.jpg',
+						'92-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Yusha, the Vindictive',
+					'rice' => 11,
+					'cards' => array(
+						'83-1.jpg',
+						'83-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Bobata',
+					'rice' => 13,
+					'cards' => array(
+						'99-1.jpg',
+						'99-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Zuba, the All Seeing',
+					'rice' => 13,
+					'cards' => array(
+						'104-1.jpg',
+						'104-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Kemono, the Savage',
+					'rice' => 15,
+					'cards' => array(
+						'94-1.jpg',
+						'94-2.jpg'
+					)
+				),
+				array(
+					'name' => 'Ushi, the Sadistic',
+					'rice' => 15,
+					'cards' => array(
+						'95-1.jpg',
+						'95-2.jpg'
+					)
+				),
 			)
 		)
 	)
