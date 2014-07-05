de8999f2e023aae66ab89efe23345c6a8572ac6d
tomholland <tomholland@d28b2937-92ed-460d-83c9-1728ac47e403>
2014-07-05 14:19:43 +0000

    About tab bar with factions, about and GCT link; added faction image support

--- trunk/generate_data.php
+++ trunk/generate_data.php
@@ -4,6 +4,7 @@ $data = array(
 	'factions' => array(
 		array(
 			'name' => 'Silvermoon Syndicate',
+			'image' => 'silvermoon.jpg',
 			'characters' => array(
 				array(
 					'name' => 'Dave',
@@ -17,30 +18,35 @@ $data = array(
 		),
 		array(
 			'name' => 'Ito Clan',
+			'image' => 'Itosymbol.jpg',
 			'characters' => array(
 				
 			)
 		),
 		array(
 			'name' => 'Temple of Ro-Kan',
+			'image' => 'symbolmonk.jpg',
 			'characters' => array(
 				
 			)
 		),
 		array(
 			'name' => 'Prefecture of Ryu',
+			'image' => 'symbolsamurai.jpg',
 			'characters' => array(
 				
 			)
 		),
 		array(
 			'name' => 'Cult of Yurei',
+			'image' => 'symbolyoma.jpg',
 			'characters' => array(
 				
 			)
 		),
 		array(
 			'name' => 'Savage Wave',
+			'image' => 'symboloni.jpg',
 			'characters' => array(
 				
 			)
